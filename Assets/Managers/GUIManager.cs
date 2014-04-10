5641f8b2103e1742f459d5c54035100ec67b3541
ShaunHoward <smh150@case.edu>
2014-04-10 15:45:59 -0400

    Added Runner as Basis
    I will change it up to look like Galaga tonight hopefully (4/10).

---
+++ Assets/Managers/GUIManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class GUIManager : MonoBehaviour {
+	
+	private static GUIManager instance;
+
+	//texts for various components of runner game
+	public GUIText boostsText, invText, distanceText, gameOverText, instructionsText, runnerText;
+	
+	void Start () {
+		instance = this;
+		GameEventManager.GameStart += GameStart;
+		GameEventManager.GameOver += GameOver;
+		gameOverText.enabled = false;
+	}
+
+	void Update () {
+		if(Input.GetButtonDown("Jump")){
+			GameEventManager.TriggerGameStart();
+		}
+	}
+	
+	private void GameStart () {
+		gameOverText.enabled = false;
+		instructionsText.enabled = false;
+		runnerText.enabled = false;
+		enabled = false;
+	}
+	
+	private void GameOver () {
+		gameOverText.enabled = true;
+		instructionsText.enabled = true;
+		enabled = true;
+	}
+
+	//sets boosts text, counter
+	public static void SetBoosts(int boosts){
+		instance.boostsText.text = "Boosts: " + boosts.ToString();
+	}
+
+	//sets distance text, counter
+	public static void SetDistance(float distance){
+		instance.distanceText.text = "Distance: " + distance.ToString("f0");
+	}
+
+	//sets invincibility text, timer
+	public static void SetInvincibility(float time){
+		instance.invText.text = "Invincibility Time: " + time.ToString ("f0");
+	}
+}
