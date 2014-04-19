50662cd0f98af8d27e88a3e4633eaffc8b5c7316
ShaunHoward <smh150@case.edu>
2014-04-18 21:24:03 -0400

    Added menus
    Have yet to make submenu fully functional, but is getting there. Need to
    create game modes.

---
+++ Assets/Done/Done_Scripts/EndGame.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Ends the space shooter game.
+ * 
+ * @authors EECS 290 Team 2
+ */
+public class EndGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/**
+	 * On click, button quits game.
+	 */
+	void OnClick() {
+		Application.Quit ();
+	}
+}
