6b4bf61fcf88afe66b39fae342d10241aa8c1bfd
Bryan Ogren <bao17@case.edu>
2014-04-20 16:40:17 -0400

    Enemies now spawn in waves
    GameControler is no longer responsible for spawning enemies. I have a
    separate script for that now: Classic_WaveSpawner. It only operates in
    Classic mode. The other modes no longer spawn enemies.

---
+++ Assets/CC Scripts/Classic_WaveSpawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Game controller for Cosmos Commander Final Project.
+ * Controls the game play and game modes of the game.
+ * 
+ * @authors EECS 290 Team 2
+ */
+public class Classic_WaveSpawner : MonoBehaviour
+{
+	public Vector3 spawnValues;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+	public GameObject[] enemyWaves;
+
+	void Start ()
+	{
+		StartCoroutine (SpawnWaves ());
+	}
+	
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+
+		for (int i = 0; i < enemyWaves.Length; i++) {
+			GameObject enemyWave = enemyWaves [i];
+			Vector3 spawnPosition = new Vector3 (0f, spawnValues.y, spawnValues.z);
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate (enemyWave, spawnPosition, spawnRotation);
+			while (enemiesArePresent()) {
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+	}
+
+	bool enemiesArePresent() {
+		if (GameObject.FindGameObjectsWithTag("Enemy").Equals(null)) {
+			return false;
+		}
+		return false;
+	}
+}
---
+++ Assets/CC Scripts/GameController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Game controller for Cosmos Commander Final Project.
+ * Controls the game play and game modes of the game.
+ * 
+ * @authors EECS 290 Team 2
+ */
+public class GameController : MonoBehaviour
+{
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+	public GUIText changeModeText;
+	
+	private bool gameOver;
+	private bool restart;
+	private int score;
+	
+	void Start ()
+	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		changeModeText.text = "";
+		score = 0;
+		UpdateScore ();
+	}
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape))
+			Application.Quit ();
+		if (restart)
+		{
+			if (Input.GetKeyDown (KeyCode.R))
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
+
+			if (Input.GetKeyDown (KeyCode.M)) //loads mode select menu
+			{
+				Application.LoadLevel (1);
+			}
+		}
+		if (gameOver)
+		{
+			restartText.text = "Press 'R' for Restart";
+			restart = true;
+			changeModeText.text = "Press 'M' for Mode Selection";
+		}
+	}
+
+		
+	
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+	
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
+	}
+	
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+	}
+}
