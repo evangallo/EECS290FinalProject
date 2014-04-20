2b0adbcfbadf432c5bd9d25c213324e87ed604d3
Bryan Ogren <bao17@case.edu>
2014-04-20 03:42:36 -0400

    Set up an AI template for dodging
    It's in EvasiveManeuver in the CC Scripts folder

--- Assets/CC Scripts/EvasiveManeuver.cs
+++ Assets/CC Scripts/EvasiveManeuver.cs
@@ -5,9 +5,11 @@ using System.Collections;
  * Evasive Maneuver Controller for Cosmos Commander Final Project.
  * Controls the maneuvers of the enemies in the game.
  * 
+ * Right now, dodgeing is random, we need them to be able to dodge asteroids and 
+ * not run into each other
  * @authors EECS 290 Team 2
  */
-public class Done_EvasiveManeuver : MonoBehaviour
+public class EvasiveManeuver : MonoBehaviour
 {
 	public Done_Boundary boundary;
 	public float tilt;
@@ -25,9 +27,31 @@ public class Done_EvasiveManeuver : MonoBehaviour
 		currentSpeed = rigidbody.velocity.z;
 		StartCoroutine(Evade());
 	}
-	
-	IEnumerator Evade ()
-	{
+
+	/**
+	 * This is where the AI should be implemented, I think. Evasive behavior is 
+	 * determined by setting targetManeuver
+	 */
+	IEnumerator Evade () {
+		/*Transform incomingAsteroid = detectAsteroid();
+		if (incomingAsteroid != null){
+			if (detectObstacleLeft() && detectObstacleRight()) {
+					//Don't dodge
+			} else if (incomingAsteroid.position.x < Transform.position.x){
+			  	if (detectObstacleRight()){
+		  				//Dodge left
+				} else {
+		  				//Dodge right
+				}
+			} else {
+			  	if (detectObstacleLeft()){
+		  				//Dodge right
+				} else {
+		  				//Dodge left
+			  	}
+			}
+		}*/
+
 		yield return new WaitForSeconds (Random.Range (startWait.x, startWait.y));
 		while (true)
 		{
@@ -50,5 +74,27 @@ public class Done_EvasiveManeuver : MonoBehaviour
 		);
 		
 		rigidbody.rotation = Quaternion.Euler (0, 0, rigidbody.velocity.x * -tilt);
+	}
+
+	/**
+	 * Detects if there is an asteroid behind the enemy and returns the asteroids transform
+	 * or null if there is no asteroid
+	 */
+	Transform detectAsteroid() {
+		return null;
+	}
+
+	/**
+	 * Detect if there is an asteroid or another enemy to this enemy's left
+	 */
+	bool detectObstacleLeft() {
+		return false;
+	}
+
+	/**
+	 * Detect if there is an asteroid or another enemy to this enemy's right
+	 */
+	bool detectObstacleRight() {
+		return false;
 	}
 }
