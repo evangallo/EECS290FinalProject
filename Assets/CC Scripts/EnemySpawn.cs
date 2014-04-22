fce5fa8eda1fd6fc1511018cbec63dc0454dc9b8
Bryan Ogren <bao17@case.edu>
2014-04-22 02:55:36 -0400

    Edited spawning script and set up waves for different modes
    Time attack and challenge work relatively well. Something about classic
    prevents the waves from cycling. Maybe the altered bolt prefab? The
    spawning for survival is fine, but the ridiculous rate of fire makes it
    too easy.

--- Assets/CC Scripts/EnemySpawn.cs
+++ Assets/CC Scripts/EnemySpawn.cs
@@ -16,14 +16,18 @@ public class EnemySpawn : MonoBehaviour
 	public float waveWait;
 	public float maxSpawnWait, minSpawnWait;
 	public float spawnWaitDecrement;
-	public int hazardCount;
 	public GameObject[] enemyWaves;
-	public float currentWait;
+
+	private float currentWait;
 	
 	void Start ()
 	{
+		currentWait = maxSpawnWait;
+
+
 		switch (gameMode) //select game mode coroutine
 		{
+
 			case "classic":
 			StartCoroutine (SpawnClassicWaves ());
 			break;
@@ -32,118 +36,69 @@ public class EnemySpawn : MonoBehaviour
 			currentWait = maxSpawnWait;
 			StartCoroutine (SpawnSurvivalWaves ());
 			break;
-
+			//Time attack and challenge use the same routine as classic.
+			//A different set of waves can be determined for each scene.
 			case "time attack":
-			StartCoroutine (SpawnTimeAttackWaves ());
+			StartCoroutine (SpawnClassicWaves ());
 			break;
 
 			case "challenge":
-			StartCoroutine (SpawnChallengeWaves ());
+			StartCoroutine (SpawnClassicWaves ());
 			break;
 		}
 	}
 
 	/**
 	 * Spawn the enemy waves for classic game mode.
+	 * Classic mode runs one cycle of a predetermined set of waves, 
+	 * spawning the next wave only once the current wave has been cleared.
 	 */
 	IEnumerator SpawnClassicWaves ()
 	{
 		yield return new WaitForSeconds (startWait);
 		
-		while (true) 
-		{
-			for (int i = 0; i < hazardCount; i++)
-			{
-				GameObject hazard = enemyWaves [Random.Range (0, enemyWaves.Length)];
-				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-				Quaternion spawnRotation = Quaternion.identity;
-				Instantiate (hazard, spawnPosition, spawnRotation);
+
+		for (int i = 0; i < enemyWaves.Length; i++){
+			GameObject enemyWave = enemyWaves [i];
+			Vector3 spawnPosition = new Vector3 (0f, spawnValues.y, spawnValues.z);
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate (enemyWave, spawnPosition, spawnRotation);
+			while (enemiesArePresent()) {
 				yield return new WaitForSeconds (spawnWait);
 			}
-
-			yield return new WaitForSeconds (Random.Range (0, waveWait));
+			yield return new WaitForSeconds (waveWait);
 		}
+
+			
 	}
 
 	/**
 	 * Spawn the enemy waves for survival game mode.
+	 * Survival mode repeatedly spawns a single enemy at decreasing time intervals.
 	 */
 	IEnumerator SpawnSurvivalWaves ()
 	{
-		yield return new WaitForSeconds (startWait);
-		
-		while (true) 
-		{
-			for (int i = 0; i < enemyWaves.Length; i++)
-			{
-				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-				Quaternion spawnRotation = Quaternion.identity;
-				Instantiate (enemyWaves[i], spawnPosition, spawnRotation);
-				yield return new WaitForSeconds (currentWait);
-				if (currentWait > minSpawnWait)
-				{
-					currentWait -= spawnWaitDecrement;
-				} else {
-					currentWait = minSpawnWait;
-				}
-			}
-			
-			//yield return new WaitForSeconds (waveWait);
-		}
-	}
 
-	/**
-	 * Spawn the enemy waves for time attack game mode.
-	 */
-	IEnumerator SpawnTimeAttackWaves ()
-	{
 		yield return new WaitForSeconds (startWait);
 		
-		while (true) 
-		{
-			for (int i = 0; i < enemyWaves.Length; i++)
-			{
-				GameObject enemyWave = enemyWaves [i];
-				Vector3 spawnPosition = new Vector3 (0f, spawnValues.y, spawnValues.z);
-				Quaternion spawnRotation = Quaternion.identity;
-				Instantiate (enemyWave, spawnPosition, spawnRotation);
-				while (enemiesArePresent()) 
-				{
-					yield return new WaitForSeconds (spawnWait);
-				}
+		while (true) {
+			Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), 
+			                                     spawnValues.y, spawnValues.z);
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate (enemyWaves[0], spawnPosition, spawnRotation);
+			yield return new WaitForSeconds (currentWait);
+			if (currentWait > minSpawnWait){
+				currentWait -= spawnWaitDecrement;
+			} else {
+				currentWait = minSpawnWait;
 			}
-			
-			yield return new WaitForSeconds (waveWait);
-		}
-	}
-
-	/**
-	 * Spawn the enemy waves for challenge game mode.
-	 */
-	IEnumerator SpawnChallengeWaves ()
-	{
-		yield return new WaitForSeconds (startWait);
-		
-		while (true) 
-		{
-			for (int i = 0; i < enemyWaves.Length; i++)
-			{
-				GameObject enemyWave = enemyWaves [i];
-				Vector3 spawnPosition = new Vector3 (0f, spawnValues.y, spawnValues.z);
-				Quaternion spawnRotation = Quaternion.identity;
-				Instantiate (enemyWave, spawnPosition, spawnRotation);
-				while (enemiesArePresent()) 
-				{
-					yield return new WaitForSeconds (spawnWait);
-				}
-			}
-			
-			yield return new WaitForSeconds (waveWait);
 		}
 	}
 
 	/**
 	 * Check whether enemies are in the viewing area.
+	 * For some reason, this causes a really long delay and I don't know why.
+	 * At least it works.
 	 */
 	bool enemiesArePresent() {
 		
