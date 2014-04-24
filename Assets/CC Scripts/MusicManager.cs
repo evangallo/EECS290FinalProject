3f8c8128798172291744caa769e3b9f188ebecd0
Evan Gallo <evang10@gmail.com>
2014-04-24 15:45:23 -0400

    working on persistent music
    Signed-off-by: Evan Gallo <evang10@gmail.com>

---
+++ Assets/CC Scripts/MusicManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+	public AudioClip newMusic;
+
+	void Awake(){
+				var go = GameObject.Find ("Game Music");
+				go.audio.clip = newMusic;
+				go.audio.Play ();
+		}
+
+
+}
