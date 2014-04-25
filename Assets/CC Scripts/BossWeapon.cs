f527261f1d246ef029944ffbf37d6401e4fd7e17
Bryan Ogren <bao17@case.edu>
2014-04-25 00:32:38 -0400

    Added Lightning beam effects to boss.

---
+++ Assets/CC Scripts/BossWeapon.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Boss Weapon controller for Cosmos Commander Final Project.
+ * Controls the weapons of the boss in the game.
+ * 
+ * @authors EECS 290 Team 2
+ */
+public class BossWeapon : MonoBehaviour
+{
+	public GameObject chargeEffect;
+	public GameObject beam;
+	public Transform beamSpawn;
+	public float chargeTime, beamTime, waitTime;
+	public float delay;
+
+	void Start ()
+	{
+		StartCoroutine(Fire());
+	}
+
+	IEnumerator Fire ()
+	{
+		yield return new WaitForSeconds (delay);
+		while (true) {
+			audio.Play ();
+			Instantiate (chargeEffect, beamSpawn.position, beamSpawn.rotation);
+			yield return new WaitForSeconds (chargeTime);
+			Instantiate (beam, beamSpawn.position, beamSpawn.rotation);
+			yield return new WaitForSeconds (beamTime);
+			yield return new WaitForSeconds (waitTime);
+		}
+	}
+}
