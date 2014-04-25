76d3a31a275a09327e61f43c0c5d624f24ac17b0
Bryan Ogren <bao17@case.edu>
2014-04-25 18:17:49 -0400

    Added range limiter to player bolts

---
+++ Assets/CC Scripts/WeaponRange.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponRange : MonoBehaviour {
+
+	public float range;
+
+	private Vector3 startPosition;
+
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	void Update () {
+		float distanceTraveled = Vector3.Magnitude (startPosition - transform.position);
+		if (distanceTraveled >= range) {
+			Destroy (this.gameObject);
+		}
+	
+	}
+}
